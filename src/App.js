ce0166455bebd12201dc29c9d464cb97bab40206
Armando Ruiz <armando.ruiz@industrie.co>
2019-08-22 16:49:59 +1000

    Creado el header y el footer y puesto para que se vea en toda la app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Productos from './components/Productos';
 import EditarProducto from './components/EditarProducto';
 import AgregarProducto from './components/AgregarProducto';
 import Producto from './components/Producto';
 
-
 function App() {
   return (
     <Router>
-      <Switch>
-        {/* Los componentes con rutas mas especificas tienen que ir primero para q no se confundan */}
-        <Route exact path="/productos/nuevo" component={AgregarProducto} />
-        <Route exact path="/productos" component={Productos} />
-        <Route exact path="/productos/:id" component={Producto} />
-        <Route exact path="/productos/editar/:id" component={EditarProducto} />
-      </Switch>
+      {/* Todo lo que se coloque entre <Router> y <Switch> se va a mostrar en todas las paginas */}
+      <Header />
+      <div className="container mt-5">
+        <Switch>
+          {/* Los componentes con rutas mas especificas tienen que ir primero para q no se confundan */}
+          <Route exact path="/productos/nuevo" component={AgregarProducto} />
+          <Route exact path="/productos" component={Productos} />
+          <Route exact path="/productos/:id" component={Producto} />
+          <Route exact path="/productos/editar/:id" component={EditarProducto} />
+        </Switch>
+      </div>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <p className="mt-4 p2 text-center text-muted">
+      Todos los derechos Reservados.
+    </p>
+  );
+};
+
+export default Footer;
