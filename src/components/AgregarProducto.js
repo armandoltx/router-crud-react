8ff801af45afd39c498d5392f2c55fefefe64f02
Armando Ruiz <armando.ruiz@industrie.co>
2019-08-26 09:49:41 +1000

    Creando el formulario y agregando el state

--- src/components/AgregarProducto.js
+++ src/components/AgregarProducto.js
@@ -1,10 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AgregarProducto = () => {
+
+  //state
+  const [ nombrePlatillo, guardarNombre ] = useState('');
+  const [ precioPlatillo, guardarPrecio ] = useState('');
+  const [ categoria, guardarCategoria ] = useState('');
+
+  const leerValorRadioCategoria = (e) => {
+    guardarCategoria(e.target.value);
+  }
+
+
   return (
-    <h1 className="text-center">
-      Agregar Nuevo Producto
-    </h1>
+    <div className="col-md-8 mx-uto">
+      <h1 className="text-center">Agregar Nuevo Producto</h1>
+      <form
+          className="mt-5"
+      >
+          <div className="form-group">
+              <label>Nombre Platillo</label>
+              <input
+                  type="text"
+                  className="form-control"
+                  name="nombre"
+                  placeholder="Nombre Platillo"
+                  onChange={e => guardarNombre(e.target.value)}
+              />
+          </div>
+
+          <div className="form-group">
+              <label>Precio Platillo</label>
+              <input
+                  type="number"
+                  className="form-control"
+                  name="precio"
+                  placeholder="Precio Platillo"
+                  onChange={e => guardarPrecio(e.target.value)}
+              />
+          </div>
+
+          <legend className="text-center">Categoría:</legend>
+          <div className="text-center">
+          <div className="form-check form-check-inline">
+              <input
+                  className="form-check-input"
+                  type="radio"
+                  name="categoria"
+                  value="postre"
+                  onChange={leerValorRadioCategoria}
+              />
+              <label className="form-check-label">
+                  Postre
+              </label>
+          </div>
+          <div className="form-check form-check-inline">
+              <input
+                  className="form-check-input"
+                  type="radio"
+                  name="categoria"
+                  value="bebida"
+                  onChange={leerValorRadioCategoria}
+              />
+              <label className="form-check-label">
+                  Bebida
+              </label>
+          </div>
+
+          <div className="form-check form-check-inline">
+              <input
+                  className="form-check-input"
+                  type="radio"
+                  name="categoria"
+                  value="cortes"
+                  onChange={leerValorRadioCategoria}
+              />
+              <label className="form-check-label">
+                  Cortes
+              </label>
+          </div>
+
+          <div className="form-check form-check-inline">
+              <input
+                  className="form-check-input"
+                  type="radio"
+                  name="categoria"
+                  value="ensalada"
+                  onChange={leerValorRadioCategoria}
+              />
+              <label className="form-check-label">
+                  Ensalada
+              </label>
+          </div>
+          </div>
+
+          <input type="submit" className="font-weight-bold text-uppercase mt-5 btn btn-primary btn-block py-3" value="Agregar Producto" />
+      </form>
+    </div>
   );
 };
 
